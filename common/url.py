6815f015f518e3b0a753442f15e6710f6c23e586
xi0225 <1015736026@qq.com>
2021-12-16 15:28:48 +0800

    删除

--- common/url.py
+++
@@ -1,7 +0,0 @@
-"""
-存放测试网站的网址
-"""
-
-
-class Url():
-    base_url = "https://www.baidu.com/"
--- page/test.py
+++
@@ -1,8 +0,0 @@
-import grpcio
-import
-import grpc
-
-def run():    # 连接 rpc 服务器    channel = grpc.insecure_channel('localhost:50051')    # 调用 rpc 服务    stub = helloworld_pb2_grpc.GreeterStub(channel)    response = stub.SayHello(helloworld_pb2.HelloRequest(name='test'))    print("Greeter client received: " + response.message)if __name__ == '__main__':    run()
-
-    python - m
-    grpc_tools.protoc - -python_out =.--grpc_python_out =.-I.helloworld.proto
